7827fff84c10873798c1c4c5e698e325f4e8b4fc
robertjota <robertjota@gmail.com>
2023-10-24 00:08:52 -0400

    Frontend template updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,15 @@
 
     <!-- Google Fonts -->
     <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,700"
-        rel="stylesheet">
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,700"
+          rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="vendor/aos/aos.css" rel="stylesheet">
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
@@ -52,18 +52,18 @@
                     <li><a class="nav-link scrollto" href="#contact">Contactos</a></li>
 
                     @if (Route::has('login'))
-                    @auth
-                    <li><a href="{{ route('dashboard') }}"
-                            class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a></li>
-                    @else
-                    <li><a href="{{ route('login') }}"
-                            class="text-sm text-gray-700 dark:text-gray-500 underline">Iniciar Sesión</a></li>
+                        @auth
+                            <li><a href="{{ route('dashboard') }}"
+                                   class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a></li>
+                        @else
+                            <li><a href="{{ route('login') }}"
+                                   class="text-sm text-gray-700 dark:text-gray-500 underline">Iniciar Sesión</a></li>
 
-                    {{-- @if (Route::has('register'))
+                            {{-- @if (Route::has('register'))
                     <li><a href="{{ route('register') }}"
                             class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registro</a></li>
                     @endif --}}
-                    @endauth
+                        @endauth
                     @endif
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
@@ -309,25 +309,25 @@
 
                     <div class="col-lg-3 col-6 text-center">
                         <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1"
-                            class="purecounter"></span>
+                              class="purecounter"></span>
                         <p>Clients</p>
                     </div>
 
                     <div class="col-lg-3 col-6 text-center">
                         <span data-purecounter-start="0" data-purecounter-end="421" data-purecounter-duration="1"
-                            class="purecounter"></span>
+                              class="purecounter"></span>
                         <p>Projects</p>
                     </div>
 
                     <div class="col-lg-3 col-6 text-center">
                         <span data-purecounter-start="0" data-purecounter-end="1364" data-purecounter-duration="1"
-                            class="purecounter"></span>
+                              class="purecounter"></span>
                         <p>Hours Of Support</p>
                     </div>
 
                     <div class="col-lg-3 col-6 text-center">
                         <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1"
-                            class="purecounter"></span>
+                              class="purecounter"></span>
                         <p>Hard Workers</p>
                     </div>
 
@@ -365,9 +365,9 @@
                                 <p>App</p>
                                 <div>
                                     <a href="img/portfolio/app1.jpg" data-gallery="portfolioGallery" title="App 1"
-                                        class="portfolio-lightbox link-preview"><i class="bi bi-plus"></i></a>
+                                       class="portfolio-lightbox link-preview"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -381,9 +381,9 @@
                                 <p>Web</p>
                                 <div>
                                     <a href="img/portfolio/web3.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="Web 3"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="Web 3"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -397,9 +397,9 @@
                                 <p>App</p>
                                 <div>
                                     <a href="img/portfolio/app2.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="App 2"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="App 2"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -413,9 +413,9 @@
                                 <p>Card</p>
                                 <div>
                                     <a href="img/portfolio/card2.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="Card 2"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="Card 2"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -429,9 +429,9 @@
                                 <p>Web</p>
                                 <div>
                                     <a href="img/portfolio/web2.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="Web 2"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="Web 2"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -445,9 +445,9 @@
                                 <p>App</p>
                                 <div>
                                     <a href="img/portfolio/app3.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="App 3"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="App 3"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -461,9 +461,9 @@
                                 <p>Card</p>
                                 <div>
                                     <a href="img/portfolio/card1.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="Card 1"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="Card 1"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -477,9 +477,9 @@
                                 <p>Card</p>
                                 <div>
                                     <a href="img/portfolio/card3.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="Card 3"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="Card 3"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -493,9 +493,9 @@
                                 <p>Web</p>
                                 <div>
                                     <a href="img/portfolio/web1.jpg" class="portfolio-lightbox link-preview"
-                                        data-gallery="portfolioGallery" title="Web 1"><i class="bi bi-plus"></i></a>
+                                       data-gallery="portfolioGallery" title="Web 1"><i class="bi bi-plus"></i></a>
                                     <a href="portfolio-details.html" class="link-details" title="More Details"><i
-                                            class="bi bi-link"></i></a>
+                                           class="bi bi-link"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -762,8 +762,8 @@
                     <div class="col-lg-6">
                         <div class="map mb-4 mb-lg-0">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-                                frameborder="0" style="border:0; width: 100%; height: 340px;" allowfullscreen></iframe>
+                                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                                    frameborder="0" style="border:0; width: 100%; height: 340px;" allowfullscreen></iframe>
                         </div>
                     </div>
 
@@ -788,20 +788,20 @@
                                 <div class="row">
                                     <div class="form-group col-lg-6">
                                         <input type="text" name="name" class="form-control" id="name"
-                                            placeholder="Your Name" required>
+                                               placeholder="Your Name" required>
                                     </div>
                                     <div class="form-group col-lg-6 mt-3 mt-lg-0">
                                         <input type="email" class="form-control" name="email" id="email"
-                                            placeholder="Your Email" required>
+                                               placeholder="Your Email" required>
                                     </div>
                                 </div>
                                 <div class="form-group mt-3">
                                     <input type="text" class="form-control" name="subject" id="subject"
-                                        placeholder="Subject" required>
+                                           placeholder="Subject" required>
                                 </div>
                                 <div class="form-group mt-3">
                                     <textarea class="form-control" name="message" rows="5" placeholder="Message"
-                                        required></textarea>
+                                              required></textarea>
                                 </div>
                                 <div class="my-3">
                                     <div class="loading">Cargando</div>
@@ -828,7 +828,7 @@
                 <div class="row">
 
                     <div class="col-lg-4 col-md-6 footer-info">
-                        <h3>RRsistemas</h3>
+                        <h3>RiaCAD Systems</h3>
                         <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita
                             valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet
                             proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
@@ -881,33 +881,24 @@
         <div class="container">
             <div class="copyright">
                 &copy; Copyright <a style="color:darkorange"
-                    href="https://rrsistemas.ml/"><strong>RRistemas</strong></a>. Todos los Derechos
+                   href="https://riacad.com/"><strong>RiaCAD Systems</strong></a>. Todos los Derechos
                 Reservados
-            </div>
-            <div class="credits">
-                <!--
-        All the links in the footer should remain intact.
-        You can delete the links only if you purchased the pro version.
-        Licensing information: https://bootstrapmade.com/license/
-        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=NewBiz
-      -->
-                Plantilla de <a href="https://bootstrapmade.com/">BootstrapMade</a>
             </div>
         </div>
     </footer><!-- End Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+           class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-    <script src="vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="vendor/aos/aos.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="vendor/php-email-form/validate.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
     <script src="js/main.js"></script>
